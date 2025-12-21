e1a8aa28490abf7383e35794508cdc732bc0539f
ravi1997 <ravi199777@gmail.com>
2025-12-21 18:20:56 +0530

    feat(brain): Optimized energy_decay_rate to 0.001900

--- brain.cpp
+++ brain.cpp
@@ -9,7 +9,7 @@
 struct BrainConfig {
     double curiosity = 0.8;
     double playfulness = 0.7;
-    double energy_decay = 0.002218; // Slower decay (~25 mins awake)
+    double energy_decay = 0.001900; // Slower decay (~25 mins awake)
     
     // "key": value,
     // "key": "value"
